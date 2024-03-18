c6f1344c7a66f0bb4507e12bd523d0dfaf39d518
Hana <hababihasen@gmail.com>
2024-03-18 11:38:56 +0300

    Added Objects

---
+++ Objects/Book Inventory.js
@@ -0,0 +1,22 @@
+let bookInventory = {
+    book1: {
+        title: "The Kingdom Of God",
+        author: "Asim Khan",
+        pages: 470,
+        available: true
+    },
+    book2: {
+        title: "The Parables of the Qur’an ",
+        author: "Yasir Qadhi",
+        pages: 530,
+        available: false
+    },
+    book3: {
+        title: "Prayers of the Pious",
+        author: "Omar Suleiman",
+        pages: 300,
+        available: true
+    }
+};
+
+console.log("Book Inventory:", bookInventory);
---
+++ Objects/User Profile.js
@@ -0,0 +1,9 @@
+
+let userProfile = {
+    name: "Aisha",
+    age: 18,
+    email: "BintAbubeker@gmail.com"
+};
+
+
+console.log("User Profile:", userProfile);
